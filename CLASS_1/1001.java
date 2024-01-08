a14b84c3e742601021d384f53f670e2a709fb44f
whodatt1 <codingman875@naver.com>
2024-01-08 21:42:34 +0900

    update:2024-01-08 21:42

---
+++ CLASS_1/1001.java
@@ -0,0 +1,45 @@
+/*
+
+Baekjoon no. 1001
+
+A-B
+ 
+시간 제한	메모리 제한	제출	정답	맞힌 사람	정답 비율
+2 초	128 MB	406483	278438	234437	69.777%
+문제
+두 정수 A와 B를 입력받은 다음, A-B를 출력하는 프로그램을 작성하시오.
+
+입력
+첫째 줄에 A와 B가 주어진다. (0 < A, B < 10)
+
+출력
+첫째 줄에 A-B를 출력한다.
+
+예제 입력 1 
+3 2
+예제 출력 1 
+1
+
+*/
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int A = Integer.parseInt(st.nextToken());
+		
+		int B = Integer.parseInt(st.nextToken());
+		
+		System.out.println(A - B);
+	}
+
+}
