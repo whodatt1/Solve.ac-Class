344855fb370f12da7f6f9b62867166187da68604
whodatt1 <codingman875@naver.com>
2024-05-21 23:18:40 +0900

    update:2024-05-21 23:18

---
+++ CLASS_2/1920.java
@@ -0,0 +1,84 @@
+/*
+    
+수 찾기 (1920번)
+
+문제
+N개의 정수 A[1], A[2], …, A[N]이 주어져 있을 때, 이 안에 X라는 정수가 존재하는지 알아내는 프로그램을 작성하시오.
+
+입력
+첫째 줄에 자연수 N(1 ≤ N ≤ 100,000)이 주어진다. 다음 줄에는 N개의 정수 A[1], A[2], …, A[N]이 주어진다. 다음 줄에는 M(1 ≤ M ≤ 100,000)이 주어진다. 다음 줄에는 M개의 수들이 주어지는데, 이 수들이 A안에 존재하는지 알아내면 된다. 모든 정수의 범위는 -231 보다 크거나 같고 231보다 작다.
+
+출력
+M개의 줄에 답을 출력한다. 존재하면 1을, 존재하지 않으면 0을 출력한다.
+
+예제 입력 1 
+5
+4 1 5 2 3
+5
+1 3 7 9 5
+예제 출력 1 
+1
+1
+0
+0
+1
+
+*/
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[N];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        int M = Integer.parseInt(br.readLine());
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < M; i++) {
+            sb.append(binarySearch(arr, Integer.parseInt(st.nextToken())));
+
+            if (i != M - 1) {
+                sb.append("\n");
+            }
+        }
+
+        System.out.println(sb);
+    }
+
+    public static int binarySearch(int[] arr, int x) {
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left <= right) {
+            int mid = (int) Math.floor((left + right) / 2);
+
+            if (arr[mid] == x) {
+                return 1;
+            } else if (arr[mid] < x) {
+                left = mid + 1;
+            } else if (arr[mid] > x) {
+                right = mid - 1;
+            }
+        }
+
+        return 0;
+    }
+}
